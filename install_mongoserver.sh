f05adf3d6e16508b854b9395f9390546df43571c
Khezen <simonneaug@gmail.com>
2016-10-01 18:25:29 +0200

    add mongotools in the build

---
+++ install_mongoserver.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# miscellaneous
+apt-get update
+apt-get install -y build-essential git binutils
+
+# RocksDB
+apt-get update
+apt-get install -y libbz2-dev libsnappy-dev zlib1g-dev libzlcore-dev
+
+git clone https://github.com/facebook/rocksdb.git
+cd rocksdb
+git checkout tags/v4.9
+CXXFLAGS="-flto -Os -s" make -j$(nproc) shared_lib
+make install
+
+# MongoDB
+apt-get update
+apt-get install -y scons
+git clone https://github.com/mongodb-partners/mongo-rocks.git /mongo-rocks
+cd /mongo-rocks
+git checkout tags/r3.2.9
+git clone https://github.com/mongodb/mongo.git /mongo
+cd /mongo
+git checkout tags/r3.2.9
+mkdir -p src/mongo/db/modules/
+ln -sf /mongo-rocks src/mongo/db/modules/rocks
+CXXFLAGS="-flto -Os -s" scons CPPPATH=/usr/local/include LIBPATH=/usr/local/lib -j$(nproc) --release --prefix=/usr --opt core  install
+
+# purge
+strip /usr/bin/mongo
+strip /usr/bin/mongod
+strip /usr/bin/mongos
+strip /usr/bin/mongoperf
+apt-get -y --purge autoremove build-essential git scons binutils
+rm -rf /rocksdb
+rm -rf /mongo-rocks
+rm -rf /mongo
+rm -f /usr/local/lib/librocksdb.a
---
+++ install_mongotools.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# miscellaneous
+apt-get update -y
+apt-get install wget git binutils -y
+wget apt-get install https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz -P /usr/local
+tar -C /usr/local -xzf /usr/local/go1.7.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# mongotools
+git clone https://github.com/mongodb/mongo-tools /mongo-tools
+cd /mongo-tools
+git checkout tags/r3.2.9
+TOOLS_PKG='github.com/mongodb/mongo-tools'
+rm -rf .gopath/
+mkdir -p .gopath/src/"$(dirname "${TOOLS_PKG}")"
+ln -sf `pwd` .gopath/src/$TOOLS_PKG
+export GOPATH=`pwd`/.gopath:`pwd`/vendor
+go build -o /usr/bin/bsondump bsondump/main/bsondump.go
+go build -o /usr/bin/mongoimport mongoimport/main/mongoimport.go
+go build -o /usr/bin/mongoexport mongoexport/main/mongoexport.go
+go build -o /usr/bin/mongodump mongodump/main/mongodump.go
+go build -o /usr/bin/mongorestore mongorestore/main/mongorestore.go
+go build -o /usr/bin/mongostat mongostat/main/mongostat.go
+go build -o /usr/bin/mongofiles mongofiles/main/mongofiles.go
+go build -o /usr/bin/mongooplog mongooplog/main/mongooplog.go
+go build -o /usr/bin/mongotop mongotop/main/mongotop.go
+
+# purge
+strip /usr/bin/bsondump
+strip /usr/bin/mongoimport
+strip /usr/bin/mongoexport
+strip /usr/bin/mongodump
+strip /usr/bin/mongorestore
+strip /usr/bin/mongostat
+strip /usr/bin/mongofiles
+strip /usr/bin/mongooplog
+strip /usr/bin/mongotop
+apt-get -y --purge autoremove  wget git binutils
+rm -rf /mongo-tools
+rm -rf /usr/local/go
+rm -f /usr/local/go1.7.1.linux-amd64.tar.gz
