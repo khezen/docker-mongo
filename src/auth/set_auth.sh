34a112235394e39fa2ccf4dc8ac0fb2f6fcbc0ef
Guillaume <guillaume@localhost.localdomain>
2016-10-18 17:17:55 +0200

    useless condition

--- src/auth/set_auth.sh
+++ src/auth/set_auth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f "$dbpath"/.auth_set ] && [ "$config_servers" == "" ]; then
+if [ ! -f "$dbpath"/.auth_set ]; then
 echo "Creating user in MongoDB..."
 mongo admin --eval "db.createUser({user: '$admin_user', pwd: '$admin_pwd', roles:[{role: 'root', db: 'admin'}]});"
 if [ "$database" != "" ]; then
