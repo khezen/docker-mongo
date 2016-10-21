1ab1fed8120d713b7a2edf0392e8d76a06d21a61
Guillaume Simonneau <milu@localhost.localdomain>
2016-10-22 00:26:14 +0200

    perf tweak only on rocksdb storage engine

--- src/miscellaneous/perf.sh
+++ src/miscellaneous/perf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ ! -f "$dbpath"/.perf_tricks_set ] && [ "$config_servers" == "" ]; then
+if [ "$storage_engine" == "rocksdb" ] && [ "$config_servers" == "" ]; then
   echo mongo --eval "db.getSiblingDB('admin').runCommand({setParameter: 1, internalQueryExecYieldPeriodMS: 1000});"
   mongo --eval "db.getSiblingDB('admin').runCommand({setParameter: 1, internalQueryExecYieldPeriodMS: 1000});"
   echo mongo --eval "db.getSiblingDB('admin').runCommand({setParameter: 1, internalQueryExecYieldIterations: 100000});"
   mongo --eval "db.getSiblingDB('admin').runCommand({setParameter: 1, internalQueryExecYieldIterations: 100000});"
-  touch "$dbpath"/.perf_tricks_set
 fi
