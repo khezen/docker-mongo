1745942697316a71cb3dd04177ffced76b1cab26
Guillaume Simonneau <milu@localhost.localdomain>
2016-12-19 13:15:41 +0100

    update rocksdb to 4.13

--- src/setup/install_mongoserver.sh
+++ src/setup/install_mongoserver.sh
@@ -9,7 +9,7 @@ apt-get update
 apt-get install -y libbz2-dev libsnappy-dev zlib1g-dev
 git clone https://github.com/facebook/rocksdb.git
 cd rocksdb
-git checkout tags/v4.11.2
+git checkout tags/v4.13.4
 CXXFLAGS="-flto -Os -s" make -j$(nproc) shared_lib
 make install
 
