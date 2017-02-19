153513fa9dbcfafda00b440bc3cfc5b4f994909f
khezen <simonneaug@gmail.com>
2017-02-19 11:43:29 +0100

    issue #7

--- src/auth/create_db_owner.sh
+++ src/auth/create_db_owner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mongoshell=$(/run/misc/mongoshell.sh)
 if [ ! -f /config/.db_owner_created ] && [ "$DB_NAME" != "" ] && [ "$CONFIG_SERVERS" == "" ]; then
-    echo $mongoshell $DB_NAME --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles:[{role: 'dbOwner', db: '$DB_NAME'}]});"
-    $mongoshell $DB_NAME --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles:[{role: 'dbOwner', db: '$DB_NAME'}]});"
+    echo $mongoshell --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles:[{role: 'dbOwner', db: '$DB_NAME'}]});" $DB_NAME
+    $mongoshell admin --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles:[{role: 'dbOwner', db: '$DB_NAME'}]});" $DB_NAME
     touch /config/.db_owner_created
 fi
