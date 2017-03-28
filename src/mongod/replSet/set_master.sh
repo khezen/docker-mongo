1682c00694a0673bdd6bb6897a398bc733f4e80d
Guillaume <simonneaug@gmail.com>
2017-03-28 16:35:06 +0200

    #11 set compose master to higher priority

--- src/mongod/replSet/set_master.sh
+++ src/mongod/replSet/set_master.sh
@@ -2,7 +2,7 @@
 mongoshell=$(/run/misc/mongoshell.sh)
 if [ "$MASTER" != "" ] && [ ! -f /config/.master_set ]; then
   echo $mongoshell admin --quiet --eval "var conf = rs.conf(); conf.members[0].host='$MASTER'; rs.reconfig(conf)"
-  $mongoshell admin --quiet --eval "var conf = rs.conf(); conf.members[0].host='$MASTER'; rs.reconfig(conf)"
+  $mongoshell admin --quiet --eval "var conf = rs.conf(); conf.members[0].host='$MASTER';conf.members[0].priority=1000; rs.reconfig(conf)"
   /run/mongod/replSet/wait_until_rs_configured.sh
   touch /config/.master_set
 fi
